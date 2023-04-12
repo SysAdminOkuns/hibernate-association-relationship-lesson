f24e44b374319419e70d227d86d36e928945e2cf
SysAdminOkuns <sysad.okundaye@gmail.com>
2023-04-12 11:44:56 -0400

    Working on mapping association as a many-to-one relationship ORM

---
+++ src/main/java/client/FindingObjectClient.java
@@ -0,0 +1,33 @@
+package client;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import util.HibernateUtil;
+import entity.Message;
+
+public class FindingObjectClient {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction txn = session.getTransaction();
+        try {
+            txn.begin();
+
+            //Finding Message[id=3L]
+            Message message = (Message) session.get(Message.class, 3L);
+            System.out.println(message);
+
+        } catch (Exception e) {
+            if (txn != null) {
+                txn.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+    }
+}
+
